cafc7a3c6323318ba19f04e8be63f03e984942c5
Andrey Glavchev <wastedabuser@gmail.com>
2015-11-06 22:23:07 +0200

    Adding docs to mail classes and some minor improvements

--- lib/Eldhelm/Mail/Bulk.pm
+++ lib/Eldhelm/Mail/Bulk.pm
@@ -1,5 +1,28 @@
 package Eldhelm::Mail::Bulk;
 
+=pod
+
+=head1 NAME
+
+Eldhelm::Mail::Bulk - Bulk mail sending via smtp.
+
+=head1 SYNOPSIS
+
+	Eldhelm::Mail::Bulk->new(
+		recipients => [
+			{ mail => 'a@abc.com' },
+			{ mail => 'b@abc.com' }
+		],
+		subject    => 'News',
+		tpl        => 'mail.news'
+	)->send;
+
+=head1 METHODS
+
+=over
+
+=cut
+
 use strict;
 use Net::SMTP::TLS;
 use Eldhelm::Util::Template;
@@ -9,6 +32,36 @@ use Data::Dumper;
 use Time::HiRes qw(usleep);
 
 our $| = 1;
+
+=item new(%args)
+
+Cosntructs a new object.
+
+C<%args> Hash - Constructor arguments;
+
+C<recipients> String - Will parse a file;
+C<from> String - Will parse a stream;
+C<subject> String - The subject of the mail;
+C<content> String - The content of the mail;
+C<tpl> String String- The mail template instead of content;
+C<tplRootPath> String - Where templates are located;
+C<tplParams> HashRef - Template compile arguments;
+C<config> HashRef - A parsed C<config.pl> file;
+C<debug> 1 or 0 or undef - Allow debugging output;
+C<limit> Number - Max number of messages to send;
+C<packetSize> Number - Max number of messages to send via one connection; Defaults to 50;
+C<packetWait> Number - How much time to wait in micro seconds before connecting for the next messages; Defaults to 250_000;
+C<printProgress> 1 or 0 or undef - Whether to output the progress of the sending;
+C<callbacks> HashRef - A hashref of subs acting as hooks to some events, see events bellow;
+C<multilanguage> 1 or 0 or undef - Whether this mail template supports multiple languages;
+C<allowedLangs> ArrayRef - A list of available languages. All others will be en;
+C<defaultLang> String - The language of the mail defaults to en;
+
+Events:
+C<onSendError> - Triggers when there is an error in the smtp sending process;
+C<onSendMail> - Triggers when the smtp accepts the message;
+
+=cut
 
 sub new {
 	my ($class, %args) = @_;
@@ -29,7 +82,7 @@ sub new {
 		callbacks     => $args{callbacks} || {},
 		multilanguage => $args{multilanguage},
 		allowedLangs  => $args{allowedLangs} || [],
-		defaultLang   => $args{defaultLang} || "en",
+		defaultLang   => $args{defaultLang} || 'en',
 	};
 	bless $self, $class;
 
@@ -38,19 +91,27 @@ sub new {
 
 sub getConfig {
 	my ($self) = @_;
-	return $self->{config}{mail} || Eldhelm::Server::Child->instance->getConfig("mail");
+	return $self->{config}{mail} || Eldhelm::Server::Child->instance->getConfig('mail');
 }
+
+=item send()
+
+Sends mail to recepients.
+
+=cut
 
 sub send {
 	my ($self) = @_;
 	my $cfg = $self->getConfig;
 	my %defaultParams = (%{ $cfg->{globalTemplateParams} }, %{ $self->{tplParams} });
-	my $size          = $self->{packetSize};
-	my %langs         = map { +$_ => $_ } @{ $self->{allowedLangs} };
-	my $limit         = 0;
-	my @list          = @{ $self->{recipients} };
+	$self->{defaultParams} = \%defaultParams;
 
-	$self->printProgress("Sending a total of ".@list." emails.\n");
+	my $size  = $self->{packetSize};
+	my %langs = map { +$_ => $_ } @{ $self->{allowedLangs} };
+	my $limit = 0;
+	my @list  = @{ $self->{recipients} };
+
+	$self->printProgress('Sending a total of '.@list." emails.\n");
 	while (@list) {
 
 		my @packet;
@@ -61,7 +122,7 @@ sub send {
 
 		last unless @packet;
 
-		$self->printProgress("Sending ".@packet." emails ...\n");
+		$self->printProgress('Sending '.@packet." emails ...\n");
 
 		my $smtp = Net::SMTP::TLS->new(
 			$cfg->{smtp}{host},
@@ -72,7 +133,7 @@ sub send {
 		);
 
 		my $i = 0;
-		foreach my $rcp (@packet) {
+	RCPLOOP: foreach my $rcp (@packet) {
 			$i++;
 
 			if (defined $self->{limit} && $limit >= $self->{limit}) {
@@ -103,13 +164,13 @@ sub send {
 			}
 			$self->printProgress(" $i($reciever;$lng;$ln)");
 
+			my $data;
 			eval {
-
 				$smtp->mail($sender);
 				$smtp->recipient($reciever);
 				$smtp->data();
 
-				my $data = "From: $sender_name <$sender> \n";
+				$data = "From: $sender_name <$sender> \n";
 				$data .= "To: <$reciever> \n";
 				$data .= qq~Content-Type: text/html; charset="utf-8" \n~;
 				$data .= "Mime-Version: 1.0 \n";
@@ -121,16 +182,28 @@ sub send {
 
 				$smtp->datasend($data);
 				$smtp->dataend();
-			};
 
-			if ($@) {
+				$self->triggerCallback(
+					'onSendMail',
+					$rcp,
+					{   subject => $subject,
+						data    => $data
+					}
+				);
+				1;
+			} or do {
 				$self->debug(Dumper $rcp);
 				$self->debug(longmess $@);
-				$self->triggerCallback("onSendError", $rcp, $@);
-				last;
-			} else {
-				$self->triggerCallback("onSendMail", $rcp);
-			}
+				$self->triggerCallback(
+					'onSendError',
+					$rcp,
+					{   subject => $subject,
+						data    => $data
+					},
+					$@
+				);
+				last RCPLOOP;
+			};
 
 		}
 
@@ -174,5 +247,19 @@ sub debug {
 	print "$msg\n";
 	return;
 }
+
+=back
+
+=head1 AUTHOR
+
+Andrey Glavchev @ Essence Ltd. (http://essenceworks.com)
+
+=head1 LICENSE
+
+This software is Copyright (c) 2011-2015 of Essence Ltd.
+
+Distributed undert the MIT license.
+ 
+=cut
 
 1;
--- lib/Eldhelm/Mail/Sender.pm
+++ lib/Eldhelm/Mail/Sender.pm
@@ -1,5 +1,25 @@
 package Eldhelm::Mail::Sender;
 
+=pod
+
+=head1 NAME
+
+Eldhelm::Mail::Sender - A simple mail sender.
+
+=head1 SYNOPSIS
+
+	Eldhelm::Mail::Sender->new(
+		to 		=> 'a@abc.com',
+		subject => 'News',
+		tpl     => 'mail.news'
+	)->send;
+
+=head1 METHODS
+
+=over
+
+=cut
+
 use strict;
 use Eldhelm::Util::Tool;
 use Eldhelm::Util::Template;
@@ -7,17 +27,35 @@ use Eldhelm::Mail::TLS;
 use MIME::Lite;
 use Data::Dumper;
 
+=item new(%args)
+
+Cosntructs a new object.
+
+C<%args> Hash - Constructor arguments;
+
+C<to> String - The recepient;
+C<from> String - The sender;
+C<replyto> String - Reply to;
+C<subject> String - The subject;
+C<content> String - The body of the mail;
+C<tpl> String - A template instead of content;
+C<tplParams> HashRef - Template compile params;
+C<config> HashRef - A parsed C<config.pl> file;
+
+=cut
+
 sub new {
 	my ($class, %args) = @_;
 	my $self = {
-		to        => $args{to},
-		from      => $args{from},
-		replyto   => $args{replyto},
-		subject   => $args{subject},
-		content   => $args{content},
-		tpl       => $args{tpl},
-		tplParams => $args{tplParams},
-		config    => $args{config},
+		to            => $args{to},
+		from          => $args{from},
+		replyto       => $args{replyto},
+		subject       => $args{subject},
+		content       => $args{content},
+		tpl           => $args{tpl},
+		tplParams     => $args{tplParams},
+		config        => $args{config},
+		defaultParams => {}
 	};
 	bless $self, $class;
 
@@ -26,8 +64,15 @@ sub new {
 
 sub getConfig {
 	my ($self) = @_;
-	return $self->{config}{mail} || Eldhelm::Server::Child->instance->getConfig("mail");
+	return $self->{config}{mail} || Eldhelm::Server::Child->instance->getConfig('mail');
 }
+
+=item send()
+
+Sends mail to hte recepient.
+Dies on error.
+
+=cut
 
 sub send {
 	my ($self) = @_;
@@ -35,21 +80,26 @@ sub send {
 	my $data;
 	if ($self->{tpl}) {
 		$data = Eldhelm::Util::Template->new(
-			name   => $self->{tpl},
-			params => Eldhelm::Util::Tool::merge({}, $cfg->{globalTemplateParams}, $self->{tplParams}),
+			name => $self->{tpl},
+			params =>
+				Eldhelm::Util::Tool::merge($self->{defaultParams}, $cfg->{globalTemplateParams}, $self->{tplParams}),
 		)->compile;
 	} else {
 		$data = $self->{content};
 	}
 
+	$self->{mail}        = $self->{to} || $cfg->{adminMail};
+	$self->{mailData}    = $data;
+	$self->{mailSubject} = sprintf($cfg->{subject}, $self->{subject});
+
 	my $cls = $cfg->{smtp}{tls} ? 'Eldhelm::Mail::TLS' : 'MIME::Lite';
 	my $mail = $cls->new(
 		From => $self->{from} || $cfg->{from},
 		$self->{replyto} ? ('Reply-to' => $self->{replyto}) : (),
-		To => $self->{to} || $cfg->{adminMail},
-		Subject  => sprintf($cfg->{subject}, $self->{subject}),
-		Encoding => "quoted-printable",
-		Type     => "text/html",
+		To       => $self->{mail},
+		Subject  => $self->{mailSubject},
+		Encoding => 'quoted-printable',
+		Type     => 'text/html',
 		Data     => $data,
 	);
 	$mail->attr('content-type.charset' => 'UTF-8');
@@ -65,5 +115,19 @@ sub send {
 		$mail->send;
 	}
 }
+
+=back
+
+=head1 AUTHOR
+
+Andrey Glavchev @ Essence Ltd. (http://essenceworks.com)
+
+=head1 LICENSE
+
+This software is Copyright (c) 2011-2015 of Essence Ltd.
+
+Distributed undert the MIT license.
+ 
+=cut
 
 1;
